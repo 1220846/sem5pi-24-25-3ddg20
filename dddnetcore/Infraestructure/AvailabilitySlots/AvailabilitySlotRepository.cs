7b952c49eec26a1024184713b5b4c563553aac5a
Rafael Ferraz <1221104@isep.ipp.pt>
2024-10-16 11:13:36 +0100

    #12 availability slots repository

---
+++ dddnetcore/Infraestructure/AvailabilitySlots/AvailabilitySlotRepository.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dddnetcore.Domain.AvailabilitySlots;
+using DDDSample1.Infrastructure;
+using DDDSample1.Infrastructure.Shared;
+
+namespace dddnetcore.Infraestructure.AvailabilitySlots
+{
+    public class AvailabilitySlotRepository : BaseRepository<AvailabilitySlot, AvailabilitySlotId>, IAvailabilitySlotRepository 
+    {
+        private readonly DDDSample1DbContext _context;
+
+        public AvailabilitySlotRepository(DDDSample1DbContext context):base(context.AvailabilitySlots) {
+            _context = context;
+        }
+    }
+}
