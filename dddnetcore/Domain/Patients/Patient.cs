f0c90847661c1459d71f458c43156737eea4e727
1220829 <1220829@isep.ipp.pt>
2024-10-18 11:55:27 +0100

    User in patient

--- dddnetcore/Domain/Patients/Patient.cs
+++ dddnetcore/Domain/Patients/Patient.cs
@@ -1,29 +1,36 @@
 using DDDSample1.Domain.Shared;
+using DDDSample1.Domain.Users;
 
 namespace DDDSample1.Domain.Patients{
     public class Patient : Entity<MedicalRecordNumber>, IAggregateRoot{
         public AppointmentHistory AppointmentHistory{get; private set;}
         public DateOfBirth DateOfBirth{get; private set;}
         public EmergencyContact EmergencyContact{get; private set;}
-        public Gender gender{get; private set;}
+        public Gender Gender{get; private set;}
         public MedicalConditions MedicalConditions{get; private set;}
         public PatientContactInformation ContactInformation{get; private set;}
         public PatientFirstName FirstName{get; private set;}
         public PatientLastName LastName{get; private set;}
         public PatientFullName FullName{get; private set;}
+
+        public User User {get; private set;}
+        public Username Username {get; private set;}
+        
         public Patient(AppointmentHistory appointmentHistory, DateOfBirth dateOfBirth, EmergencyContact emergencyContact,
             Gender gender, MedicalConditions medicalConditions, PatientContactInformation contactInformation, 
-            PatientFirstName firstName, PatientLastName lastName, PatientFullName fullName){
+            PatientFirstName firstName, PatientLastName lastName, PatientFullName fullName, User user){
             this.AppointmentHistory = appointmentHistory;
             this.DateOfBirth = dateOfBirth;
             this.EmergencyContact = emergencyContact;
-            this.gender = gender;
+            this.Gender = gender;
             this.MedicalConditions = medicalConditions;
             this.ContactInformation = contactInformation;
             this.FirstName = firstName;
             this.LastName = lastName;
             this.FullName = fullName;
+            this.User = user;
         }
+        
 
         public override bool Equals(object obj)
         {
