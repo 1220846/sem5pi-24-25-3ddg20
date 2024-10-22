319e2e39c715318d89f9a631892b7c0dd5ba2e3c
SerginhoMoreira <118183187+SerginhoMoreira@users.noreply.github.com>
2024-10-22 17:48:48 +0100

    docs: Process Views #19

--- dddnetcore/Domain/OperationRequests/OperationRequestService.cs
+++ dddnetcore/Domain/OperationRequests/OperationRequestService.cs
@@ -17,25 +17,25 @@ namespace DDDSample1.Domain.OperationRequests
 
         private readonly IUnitOfWork _unitOfWork;
         
-        private readonly IOperationRequestRepository _repoOp;
+        private readonly IOperationRequestRepository _repoOperationRequest;
 
-        private readonly IOperationTypeRepository _repoOpTy;
+        private readonly IOperationTypeRepository _repoOperationType;
 
-        private readonly IPatientRepository _repoPat;
+        private readonly IPatientRepository _repoPatient;
 
-        private readonly IStaffRepository _repoS;
+        private readonly IStaffRepository _repoStaff;
 
         public OperationRequestService(IUnitOfWork unitOfWork, IOperationRequestRepository repo, IStaffRepository repoS, IOperationTypeRepository repoOpTy, IPatientRepository repoPat){
             this._unitOfWork = unitOfWork;
-            this._repoOp = repo;
-            this._repoS=repoS;
-            this._repoOpTy=repoOpTy;
-            this._repoPat=repoPat;
+            this._repoOperationRequest = repo;
+            this._repoStaff=repoS;
+            this._repoOperationType=repoOpTy;
+            this._repoPatient=repoPat;
         }
 
         public async Task<OperationRequestDto> GetByIdAsync(OperationRequestId id)
         {
-            var operationRequest = await this._repoOp.GetByIdAsync(id);
+            var operationRequest = await this._repoOperationRequest.GetByIdAsync(id);
             
             if(operationRequest == null)
                 return null;
@@ -45,24 +45,28 @@ namespace DDDSample1.Domain.OperationRequests
 
         public async Task<OperationRequestDto> AddOperationRequestAsync(CreatingOperationRequestDto dto)
         {
-            var doctor=await _repoS.GetByIdAsync(new StaffId(dto.DoctorId));
-            var operationType= await _repoOpTy.GetByIdAsync(new OperationTypeId(dto.OperationTypeId));
+            var doctor=(await _repoStaff.GetStaffsAsync(id: dto.DoctorId)).FirstOrDefault();
+            Console.WriteLine(doctor.ToString());
+            var operationType= await _repoOperationType.GetByIdAsync(new OperationTypeId(dto.OperationTypeId));
+            Console.WriteLine(operationType);
             if(doctor==null){
                     throw new NullReferenceException("The doctor with that Id does not exist!");
             }
-            Console.WriteLine(doctor.Specialization.Id.AsString());
-            Console.WriteLine(operationType.OperationTypeSpecializations.First().Specialization.Id.AsString());
             if(operationType.OperationTypeSpecializations.Any(specialization => specialization.Specialization.Id == doctor.Specialization.Id)){
-                /*var patient = await _repoPat.GetByIdAsync(new MedicalRecordNumber(dto.MedicalRecordNumber));
+                var patient = await _repoPatient.GetByIdAsync(new MedicalRecordNumber(dto.MedicalRecordNumber));
                 if(patient==null){
                     throw new NullReferenceException("The patient with that Id does not exist!");
-                }*/
+                }
                 var priority = Enum.Parse<Priority>(dto.Priority.ToUpper());
                 var operationRequest = new OperationRequest(new MedicalRecordNumber(dto.MedicalRecordNumber), new StaffId(dto.DoctorId), new OperationTypeId(dto.OperationTypeId), DeadlineDate.FromString(dto.Deadline), priority);
-                await _repoOp.AddAsync(operationRequest);
+                await _repoOperationRequest.AddAsync(operationRequest);
                 await this._unitOfWork.CommitAsync();
-                return new OperationRequestDto {DoctorId = operationRequest.StaffId.AsString(), OperationTypeId=operationRequest.OperationTypeId.AsString(), MedicalRecordNumber=operationRequest.MedicalRecordNumber.AsString(),
-                    Deadline=operationRequest.DeadlineDate.ToString(), Priority=operationRequest.Priority.ToString(), Status=operationRequest.Status.ToString()};
+                return new OperationRequestDto {DoctorId = operationRequest.StaffId.AsString(), 
+                        OperationTypeId=operationRequest.OperationTypeId.AsString(), 
+                        MedicalRecordNumber=operationRequest.MedicalRecordNumber.AsString(),
+                        Deadline=operationRequest.DeadlineDate.ToString(), 
+                        Priority=operationRequest.Priority.ToString(), 
+                        Status=operationRequest.Status.ToString()};
             }else {
                 throw new BusinessRuleValidationException("The Doctor Specialization not match with the Operation Type");
             }
---
+++ docs/sprint-a/us-5.1.19/readme.md
@@ -0,0 +1,31 @@
+# US 5.1.19 - As a Doctor, I want to list/search operation requisitions.
+
+## 1. Context
+
+In this task it was proposed that a Doctor can list or search an operation requesitions.
+
+## 2. Requirements
+
+*US 5.1.19* As a Doctor, I want to list/search operation requisitions, so that I see the details, edit, and remove operation requisitions
+
+Acceptance Criteria:
+
+ - Doctors can search operation requests by patient name, operation type, priority, and status.
+ - The system displays a list of operation requests in a searchable and filterable view.
+ - Each entry in the list includes operation request details (e.g., patient name, operation type,
+   status).
+ - Doctors can select an operation request to view, update, or delete it.
+
+## 3. Views
+
+### Level 1
+
+![Process view level 1]()
+
+### Level 2
+
+![Process view level 2]()
+
+### Level 3
+
+![Process view level 3]()
