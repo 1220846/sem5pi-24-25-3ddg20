6b2f777d34740286f8883a9036736f36a7875761
SerginhoMoreira <118183187+SerginhoMoreira@users.noreply.github.com>
2024-10-24 18:10:05 +0100

    Implementation #17

--- dddnetcore/Domain/OperationRequests/OperationRequestService.cs
+++ dddnetcore/Domain/OperationRequests/OperationRequestService.cs
@@ -11,6 +11,8 @@ using DDDSample1.Domain.Patients;
 using DDDSample1.DataAnnotations.Patients;
 using System.Linq;
 using System.Reflection.Metadata.Ecma335;
+using dddnetcore.Domain.OperationRequests.UpdateOperationRequestDto;
+using DDDSample1.Domain.SystemLogs;
 
 namespace DDDSample1.Domain.OperationRequests
 {
@@ -20,28 +22,36 @@ namespace DDDSample1.Domain.OperationRequests
         
         private readonly IOperationRequestRepository _repoOperationRequest;
 
+        private readonly ISystemLogRepository _repoSystemLog;
+
         private readonly IOperationTypeRepository _repoOperationType;
 
         private readonly IPatientRepository _repoPatient;
 
         private readonly IStaffRepository _repoStaff;
 
-        public OperationRequestService(IUnitOfWork unitOfWork, IOperationRequestRepository repo, IStaffRepository repoS, IOperationTypeRepository repoOpTy, IPatientRepository repoPat){
+        public OperationRequestService(IUnitOfWork unitOfWork, IOperationRequestRepository repo, IStaffRepository repoS, IOperationTypeRepository repoOpTy, IPatientRepository repoPat, ISystemLogRepository systemLogRepository){
             this._unitOfWork = unitOfWork;
             this._repoOperationRequest = repo;
             this._repoStaff=repoS;
             this._repoOperationType=repoOpTy;
             this._repoPatient=repoPat;
+            this._repoSystemLog=systemLogRepository;
         }
 
         public async Task<OperationRequestDto> GetByIdAsync(OperationRequestId id)
         {
             var operationRequest = await this._repoOperationRequest.GetByIdAsync(id);
-            
             if(operationRequest == null)
                 return null;
 
-            return new OperationRequestDto{Id=operationRequest.Id.AsGuid(), DoctorId=operationRequest.StaffId.AsString(), OperationTypeId=operationRequest.OperationTypeId.ToString(), MedicalRecordNumber=operationRequest.MedicalRecordNumber.AsString(), Deadline=operationRequest.DeadlineDate.ToString(), Priority=operationRequest.Priority.ToString(), Status=operationRequest.Status.ToString()};
+            return new OperationRequestDto{Id = operationRequest.Id.AsGuid(),
+                        DoctorId = operationRequest.StaffId.Id, 
+                        OperationTypeId=operationRequest.OperationTypeId.Value, 
+                        MedicalRecordNumber=operationRequest.MedicalRecordNumber.Id,
+                        Deadline=operationRequest.DeadlineDate.Date.ToString(), 
+                        Priority=operationRequest.Priority.ToString(), 
+                        Status=operationRequest.Status.ToString()};
         }
 
         public async Task<OperationRequestDto> AddOperationRequestAsync(CreatingOperationRequestDto dto)
@@ -87,5 +97,53 @@ namespace DDDSample1.Domain.OperationRequests
             return operationRequestsDto;
         }
             
+
+        public async Task<OperationRequestDto> UpdateOperationRequestAsync(Guid operationRequestId, UpdateOperationRequestDto updateOperationRequestDto){
+            
+            var operationRequest=await this._repoOperationRequest.GetByIdAsync(new OperationRequestId(operationRequestId));
+          
+
+            if(operationRequest==null){
+                throw new BusinessRuleValidationException("The operation Request Inserted doesn't exists!");
+            }
+            var operationRequestChanges=new List<string>();
+
+            if(updateOperationRequestDto.Priority != null){
+                if (Enum.TryParse<Priority>(updateOperationRequestDto.Priority.ToUpper(), out var priority))
+                {
+                    operationRequestChanges.Add($"Priority changed from {operationRequest.Priority.ToString()} to {updateOperationRequestDto.Priority}");
+                    operationRequest.ChangePriority(priority);
+                }
+                else
+                {
+                    throw new BusinessRuleValidationException("Invalid priority value.");
+                }
+            }
+
+            if(updateOperationRequestDto.Deadline != null){
+                operationRequestChanges.Add($"Last Date changed from {operationRequest.DeadlineDate.Date.ToString()} to {updateOperationRequestDto.Deadline.ToString()}");
+                operationRequest.ChangeDeadline(DeadlineDate.FromString(updateOperationRequestDto.Deadline));
+            }
+
+            await _repoOperationRequest.UpdateOperationRequestAsync(operationRequest);
+
+            if (operationRequestChanges.Count > 0){
+                string operationRequestLog = string.Join(", ", operationRequestChanges);
+                await this._repoSystemLog.AddAsync(new SystemLog(Operation.UPDATE, Entity.OPERATION_REQUEST, operationRequestLog,operationRequest.Id.AsGuid().ToString()));
+                Console.WriteLine(operationRequest.Id.AsGuid().ToString());
+            }
+
+           
+            await this._unitOfWork.CommitAsync();
+
+
+            return new OperationRequestDto{Id = operationRequest.Id.AsGuid(),
+                        DoctorId = operationRequest.StaffId.Id, 
+                        OperationTypeId=operationRequest.OperationTypeId.Value, 
+                        MedicalRecordNumber=operationRequest.MedicalRecordNumber.Id,
+                        Deadline=operationRequest.DeadlineDate.Date.ToString(), 
+                        Priority=operationRequest.Priority.ToString(), 
+                        Status=operationRequest.Status.ToString()};
+        }
     }
 }
---
+++ dddnetcore/Domain/OperationRequests/UpdateOperationRequestDto.cs
@@ -0,0 +1,8 @@
+namespace dddnetcore.Domain.OperationRequests.UpdateOperationRequestDto{
+    public class UpdateOperationRequestDto
+    {
+        public string Deadline {get;set;}
+
+        public string Priority{get;set;}
+    }
+}
