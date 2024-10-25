bf91e7f34f0029a8153fe7b72884e15b904fcea0
Rafael Ferraz <1221104@isep.ipp.pt>
2024-10-25 17:21:36 +0100

    #18 us funcional

--- dddnetcore/Domain/Auth/AuthenticationService.cs
+++ dddnetcore/Domain/Auth/AuthenticationService.cs
@@ -1,3 +1,6 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,6 +31,23 @@ namespace DDDSample1.Domain.Auth
             var content = await response.Content.ReadAsStringAsync();
             dynamic tokenResponse = JsonConvert.DeserializeObject(content);
             return tokenResponse.access_token;
+        }
+
+        public string GetLoggedInUsername(string token)
+        {
+            if (string.IsNullOrEmpty(token))
+                throw new InvalidOperationException("Token not found");
+
+            var handler = new JwtSecurityTokenHandler();
+            var jwtToken = handler.ReadJwtToken(token);
+
+            // Aqui, 'sub' é um claim comum em tokens para identificar o usuário
+            var usernameClaim = jwtToken.Claims.FirstOrDefault(claim => claim.Type == "sub");
+            
+            if (usernameClaim == null)
+                throw new InvalidOperationException("Username not found in token claims");
+
+            return usernameClaim.Value["auth0|".Length..];
         }
     }
 }
---
+++ dddnetcore/Domain/OperationRequests/RemoveOperationRequestDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dddnetcore.Domain.OperationRequests
+{
+    public class RemoveOperationRequestDto
+    {
+        public string token {get;set;}
+    }
+}
