5e22308fd0dac732daf86b766d2ba02ede331e21
1220846 <1220846@isep.ipp.pt>
2024-10-30 11:15:25 +0000

    add sidebar and directory for services #37

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,11 @@
 import { Routes } from '@angular/router';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 export const routes: Routes = [
-    
+  { path: 'home', component: HomePageComponent },
+  { path: 'sidebar', component: SidebarComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
+
+
