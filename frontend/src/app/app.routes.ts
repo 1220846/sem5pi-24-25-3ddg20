79f3bac1a807314807bd5c4af289f14ffa2ff130
SerginhoMoreira <118183187+SerginhoMoreira@users.noreply.github.com>
2024-11-02 23:30:12 +0000

    AppRoutes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,12 +2,13 @@ import { Routes } from '@angular/router';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AdminComponent } from './pages/admins/admin.component';
+import { DoctorsComponent } from './pages/doctors/doctors.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomePageComponent },
+  { path: 'doctor', component: DoctorsComponent },
   { path: 'sidebar', component: SidebarComponent },
   { path: 'admin', component: AdminComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
-
 
