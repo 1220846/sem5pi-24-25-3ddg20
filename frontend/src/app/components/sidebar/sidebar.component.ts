304250d6bf490db010179ce89d30f1ac6e6909e3
SerginhoMoreira <118183187+SerginhoMoreira@users.noreply.github.com>
2024-11-02 23:29:06 +0000

    SideBarCreation

--- frontend/src/app/components/sidebar/sidebar.component.ts
+++ frontend/src/app/components/sidebar/sidebar.component.ts
@@ -1,33 +1,39 @@
-import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
-import { ButtonModule } from 'primeng/button'; 
-import { ToolbarModule } from 'primeng/toolbar'; 
-import { MenuModule } from 'primeng/menu'; 
+import { Component, Input, ViewChild } from '@angular/core';
+import { SidebarModule } from 'primeng/sidebar';
+import { ButtonModule } from 'primeng/button';
 import { MenubarModule } from 'primeng/menubar';
+import { RippleModule } from 'primeng/ripple';
+import { AvatarModule } from 'primeng/avatar';
+import { StyleClassModule } from 'primeng/styleclass';
+import { Sidebar } from 'primeng/sidebar';
+import { Router, RouterModule } from '@angular/router';
+import { ImageModule } from 'primeng/image';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterModule,
-    ButtonModule,
-    ToolbarModule,
-    MenuModule,
-    MenubarModule
-  ],
+  imports: [SidebarModule, ButtonModule, ImageModule ,RippleModule, AvatarModule, StyleClassModule,RouterModule,MenubarModule,CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
   @Input() userType: string = 'user';
+  userTitle: string='User Dashboard';
   items = [
-    { label: 'Início', icon: 'home', link: '/home' },
-    { label: 'Sobre', icon: 'info', link: '/about' },
-    { label: 'Serviços', icon: 'build', link: '/services' },
+    { label: 'Início', icon: 'home', link: '/sidebar' },
+    { label: 'Sobre', icon: 'spin pi-info', link: '/about' },
+    { label: 'Serviços', icon: 'spin pi-cog', link: '/services' },
     { label: 'Contato', icon: 'contact_mail', link: '/contact' }
   ];
+
+  @ViewChild('sidebarRef') sidebarRef!: Sidebar;
+
+    closeCallback(e:Event): void {
+        this.sidebarRef.close(e);
+    }
+
+    sidebarVisible: boolean = false;
 
   username: string = 'Username';
   isSidenavOpen: boolean = true;
@@ -36,6 +42,24 @@ export class SidebarComponent {
 
   logout() {
     this.router.navigate(['/login']);
+  }
+
+  setUserTitle(user: string) {
+    if (user === 'doctor') {
+      this.userTitle = 'Doctor Dashboard';
+    } else if (user === 'patient') {
+      this.userTitle = 'Patient Dashboard';
+    }else if (user === 'Admin') {
+      this.userTitle = 'Admin Dashboard';
+    }else if (user === 'Technitiant') {
+      this.userTitle = 'Technitiant Dashboard';
+    }else if (user === 'Nurse') {
+      this.userTitle = 'Nurse Dashboard';
+    }
+  }
+
+  setUserType(usernumber: string){
+    this.username=usernumber;
   }
 
   toggleSidenav() {
