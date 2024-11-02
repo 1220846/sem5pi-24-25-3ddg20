60503d4a3b0f953e09e5ea96d282fa2de03118ee
SerginhoMoreira <118183187+SerginhoMoreira@users.noreply.github.com>
2024-11-02 23:41:20 +0000

    Sidebar-Admins

--- frontend/src/app/pages/admins/admin.component.ts
+++ frontend/src/app/pages/admins/admin.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from "../../components/sidebar/sidebar.component";
 import { ModalCreateOperationTypeComponent } from "./operation-types/modal-create-operation-type/modal-create-operation-type.component";
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [ModalCreateOperationTypeComponent],
+  imports: [ModalCreateOperationTypeComponent,SidebarComponent],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss'
 })
