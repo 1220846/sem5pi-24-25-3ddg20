6bebe6e0f51c97acee8e39105515f8bbcfc387ee
1220846 <1220846@isep.ipp.pt>
2024-11-05 18:45:43 +0000

    add methods login and logout #46

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { CreatingUserPatientDto } from '../domain/CreatingUserPatientDto';
 import { User } from '@auth0/auth0-angular';
+import { LoginRequestDto } from '../domain/LoginRequestDto';
 
 @Injectable({
   providedIn: 'root'
@@ -18,5 +19,23 @@ export class UserService {
 
   createUserPatient(user: CreatingUserPatientDto):Observable<User>{
     return this.httpClient.post<User>(`${this.apiUrl}/patients`, user, { headers: this.header });
+  }
+
+  login(loginRequest: LoginRequestDto): Observable<any> {
+    return this.httpClient.post(`${this.apiUrl}/login`, loginRequest, { headers: this.header })
+      .pipe(tap((response: any) => {
+            localStorage.setItem('accessToken', response.token);
+            localStorage.setItem('roles', JSON.stringify(response.roles));
+        })
+      );
+  }
+
+  isAuthenticated(): boolean {
+    return !!localStorage.getItem('accessToken');
+  }
+
+  logout(): void {
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('roles');
   }
 }
