d87b8dd5695353522406e8981f07bc58952588cd
Rafael Ferraz <1221104@isep.ipp.pt>
2024-11-11 16:28:29 +0000

    #51 added confirmation notification #54 almost done (missing some details + pagination)

--- frontend/src/app/components/admins/staffs/staffs.component.ts
+++ frontend/src/app/components/admins/staffs/staffs.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { CreateStaffProfileComponent } from './modal-create-staff-profile/modal-create-staff-profile.component';
+import { ListStaffProfilesComponent } from './list-staff-profiles/list-staff-profiles.component';
 
 @Component({
   selector: 'app-staffs',
   standalone: true,
-  imports: [],
+  imports: [CreateStaffProfileComponent, ListStaffProfilesComponent],
   templateUrl: './staffs.component.html',
   styleUrl: './staffs.component.scss'
 })
 export class StaffsComponent {
+  @ViewChild(CreateStaffProfileComponent) modalCreateStaffProfileComponent!: CreateStaffProfileComponent;
+  @ViewChild(ListStaffProfilesComponent, { static: false}) listStaffProfilesComponent!: ListStaffProfilesComponent;
 
+  onStaffProfileCreated() {
+    this.listStaffProfilesComponent.loadStaffs();
+  }
 }
