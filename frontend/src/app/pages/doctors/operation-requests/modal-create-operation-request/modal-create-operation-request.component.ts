7f58698203accde484b7fb6e884cbd3fff47ac5d
SerginhoMoreira <118183187+SerginhoMoreira@users.noreply.github.com>
2024-11-13 14:23:38 +0000

    Implementation #55 #58

--- frontend/src/app/pages/doctors/operation-requests/modal-create-operation-request/modal-create-operation-request.component.ts
+++ frontend/src/app/pages/doctors/operation-requests/modal-create-operation-request/modal-create-operation-request.component.ts
@@ -1,46 +1,126 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { TagModule } from 'primeng/tag';
 import { InputTextModule } from 'primeng/inputtext';
-import { FormsModule } from '@angular/forms';
-import { FloatLabelModule } from 'primeng/floatlabel';
-import { CalendarModule } from 'primeng/calendar';
 import { DropdownModule } from 'primeng/dropdown';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
 import { DialogModule } from 'primeng/dialog';
-import { SplitButtonModule } from 'primeng/splitbutton';
+import { CalendarModule } from 'primeng/calendar';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
-
-
-interface Options {
-  label: string;
-}
+import { OperationRequestService } from '../../../../services/operation-request.service';
+import { CreatingOperationRequestDto } from '../../../../domain/CreatingOperationRequestDto';
+import { OperationType } from '../../../../domain/OperationType';
+import { OperationTypeService } from '../../../../services/operation-type.service';
+import { Patient } from '../../../../domain/Patient';
+import { PatientService } from '../../../../services/patient.service';
 
 @Component({
-  selector: 'modal-create-operation-request',
+  selector: 'app-modal-create-operation-request',
   standalone: true,
-  imports: [InputTextModule, FormsModule, FloatLabelModule,CalendarModule,DropdownModule,DialogModule,SplitButtonModule],
+  imports: [
+    TagModule, InputTextModule, DropdownModule, CommonModule, CalendarModule,
+    ButtonModule, DialogModule, FormsModule, ReactiveFormsModule, ToastModule
+  ],
+  providers: [MessageService],
   templateUrl: './modal-create-operation-request.component.html',
-  styleUrl: './modal-create-operation-request.component.scss'
+  styleUrls: ['./modal-create-operation-request.component.scss']
 })
-export class ModalCreateOperationRequestComponent implements OnInit{
-  DoctorId: string | undefined;
-  OperationTypeId: string | undefined;
-  MedicalRecordNumber: string | undefined;
-  Deadline: Date | undefined;
-  Priority: string | undefined;
-  options: Options[] | undefined;
+export class ModalCreateOperationRequestComponent implements OnInit {
 
-  selectedPriority: Options | undefined;
+  operationRequestForm: FormGroup;
+  @Output() operationRequestCreated = new EventEmitter<CreatingOperationRequestDto>();
 
-  ngOnInit() {
-    this.options = [
-        {label:'Elective'},
-        {label:'Urgent'},
-        {label:'Emergency'}
-    ];
+  operationTypes: OperationType[] = [];
+  availableOperationTypes: OperationType[] = [];
+
+  patients: Patient[] = [];
+  availablePatients: Patient[] = [];
+  
+  prioritySelected: string | undefined;
+
+  constructor(
+    private fb: FormBuilder,
+    private messageService: MessageService,
+    private operationRequestService: OperationRequestService,
+    private operationTypesService: OperationTypeService,
+    private patientsService: PatientService
+  ) {
+    this.operationRequestForm = this.fb.group({
+      doctorId: ['',Validators.required],
+      operationType: ['', Validators.required],
+      patient: ['', Validators.required],
+      selectedPriority: [null, Validators.required],
+      deadline: ['', Validators.required]
+    });
   }
 
+  optionsPriority = [
+    { label: 'Elective', value: 'ELECTIVE' },
+    { label: 'Urgent', value: 'URGENT' },
+    { label: 'Emergency', value: 'EMERGENCY' }
+  ];
+  
   visible: boolean = false;
 
-    showDialog() {
-        this.visible = true;
+  ngOnInit(): void {
+    this.loadOperationTypes();
+    this.loadPatients();
+  }
+
+  showDialog() {
+    this.visible = true;
+    this.resetForm();
+  }
+
+  loadPatients(){
+    this.patientsService.getAll().subscribe((data) => {
+      this.patients = data;
+      this.availablePatients = [...this.patients];
+    });
+  }
+
+  loadOperationTypes() {
+    this.operationTypesService.getAll().subscribe((data) => {
+      this.operationTypes = data;
+      this.availableOperationTypes = [...this.operationTypes];
+    });
+  }
+
+  resetForm() {
+    this.operationRequestForm.reset({
+      selectedPriority: null  
+    });
+  }  
+
+  saveData() {
+    if (this.operationRequestForm.valid) {
+      const operationRequest: CreatingOperationRequestDto = {
+        doctorId: this.operationRequestForm.value.doctorId,
+        operationTypeId: this.operationRequestForm.value.operationType,
+        medicalRecordNumber: this.operationRequestForm.value.medicalRecordNumber,
+        priority: this.operationRequestForm.value.selectedPriority,
+        deadline: this.operationRequestForm.value.deadline
+      };
+
+      this.operationRequestService.add(operationRequest).subscribe(
+        (response) => {
+          this.visible = false;
+          this.operationRequestForm.reset({
+            selectedPriority: null
+          });
+          this.operationRequestCreated.emit(operationRequest);
+          this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Operation request created successfully', life: 2000 });
+        },
+        (error) => {
+          console.error("Error creating operation request:", error);
+          this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Failed to create operation request', life: 2000 });
+        }
+      );
+    } else {
+      this.messageService.add({ severity: 'warn', summary: 'Warning', detail: 'Form is invalid. Please fill all required fields.', life: 2000 });
     }
+  }
+
 }
