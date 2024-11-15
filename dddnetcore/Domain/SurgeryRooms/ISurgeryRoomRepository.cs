06c3b409e7f0de024bcc1e7cfc428935dcce763f
Rafael Ferraz <1221104@isep.ipp.pt>
2024-11-15 15:37:48 +0000

    #63 irep

---
+++ dddnetcore/Domain/SurgeryRooms/ISurgeryRoomRepository.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DDDSample1.Domain.Shared;
+
+namespace dddnetcore.Domain.SurgeryRooms
+{
+    public class ISurgeryRoomRepository : IRepository<SurgeryRoom, RoomNumber>
+    {
+        
+    }
+}
