39eda3301905e2f34e4ab8556641cb593add232e
SerginhoMoreira <118183187+SerginhoMoreira@users.noreply.github.com>
2024-11-17 17:48:31 +0000

    Update Operation Request #56

--- frontend/src/app/pages/doctors/operation-requests/modal-update-operation-requests/modal-update-operation-requests.component.ts
+++ frontend/src/app/pages/doctors/operation-requests/modal-update-operation-requests/modal-update-operation-requests.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule , Validators} from '@angular/forms';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { CalendarModule } from 'primeng/calendar';
+import { ToastModule } from 'primeng/toast';
 import { DropdownModule } from 'primeng/dropdown';
 import { DialogModule } from 'primeng/dialog';
 
@@ -13,7 +14,7 @@ interface Options {
 @Component({
   selector: 'modal-update-operation-requests',
   standalone: true,
-  imports: [InputTextModule, FormsModule, FloatLabelModule,CalendarModule,DropdownModule,DialogModule],
+  imports: [InputTextModule, FormsModule, FormGroup,ToastModule ,FloatLabelModule,CalendarModule,DropdownModule,DialogModule],
   templateUrl: './modal-update-operation-requests.component.html',
   styleUrls: ['./modal-update-operation-requests.component.scss']
 })
@@ -22,8 +23,17 @@ export class ModalUpdateOperationRequestsComponent implements OnInit{
   Priority: string | undefined;
   MedicalRecordNumber: string | undefined;
   options: Options[] | undefined;
-
+  operationRequestForm: FormGroup;
   selectedPriority: Options | undefined;
+
+  constructor(private fb: FormBuilder
+    
+  ){
+    this.operationRequestForm = this.fb.group({
+      selectedPriority: [null, Validators.required],
+      deadline: ['', Validators.required]
+    });
+  }
 
   ngOnInit() {
     this.options = [
