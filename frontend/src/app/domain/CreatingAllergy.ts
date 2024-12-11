61ba8405ce47d829367f75732c3eb76bf9b93b80
SerginhoMoreira <118183187+SerginhoMoreira@users.noreply.github.com>
2024-12-11 15:24:31 +0000

    Correction of implementation of us-7.2.2 DOMAIN #101

---
+++ frontend/src/app/domain/CreatingAllergy.ts
@@ -0,0 +1,5 @@
+export interface CreatingAllergyDto {
+    code: string;
+    designation: string;
+    description: string;
+}
---
+++ frontend/src/app/domain/CreatingMedicalConditionDto.ts
@@ -0,0 +1,5 @@
+export interface CreatingMedicalConditionDto {
+    code: string;
+    designation: string;
+    description: string;
+}
